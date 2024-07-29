b7f7bcd8ad5a75023ea68af4add7dd00b80b3786
Marc <mr.moreaux@gmail.com>
2024-07-29 12:42:51 +0200

    Add db to files in stlite

--- docs/streamlit_app.py
+++ docs/streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from src import read_winds
 
 
 st.write("Wind in Silvaplana since August 2024")
 st.write("Source: https://www.kitesailing.ch/en/spot/webcam")
 
-# db = pd.read_csv('../db/winds.csv', encoding='latin1', on_bad_lines='skip')
-# db = db.set_index('timestamp')
-# db = read_winds.WindReader().load_db()
-# chart_data = pd.DataFrame(db)
-# st.line_chart(chart_data)
+# Read db
+db = pd.read_csv('winds.csv', encoding='latin1', on_bad_lines='skip')
+db = db.set_index('timestamp')
+
+# Plot DB
+chart_data = pd.DataFrame(db)
+st.line_chart(chart_data)
