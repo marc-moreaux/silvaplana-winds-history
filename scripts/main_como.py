9e65926416f0c935a877f1d32d083c5c211890a4
Marc <mr.moreaux@gmail.com>
2024-08-22 11:54:51 +0200

    Start gathering more winds in como

--- scripts/main_como.py
+++ scripts/main_como.py
@@ -5,3 +5,17 @@ if True:
     from src.como_reader import ComoReader
 
 ComoReader('Dervio').update_db()
+# ComoReader('Perledo_Cantone').update_db()
+ComoReader('Oliveto_Lario').update_db()
+
+ComoReader('Gera_Lario').update_db()  # 2
+'''
+ComoReader('Gera_Lario').update_db()  # 2
+ComoReader('Pian_di_Spagna').update_db()  # 2
+ComoReader('Gravedona').update_db()  # 2
+ComoReader('Dongo').update_db()  # 2
+ComoReader('Ossuccio').update_db()  # 2
+ComoReader('Mandello').update_db()  # 2
+ComoReader('Lecco').update_db()  # 2
+ComoReader('Garlate').update_db()  # 2
+'''
